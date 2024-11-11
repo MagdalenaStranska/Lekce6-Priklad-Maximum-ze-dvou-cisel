49383147368f0ecc074d26f4ab108d834a3f1e39
MagdalenaStranska <stranska.magdalena@gmail.com>
2024-11-11 17:42:30 +0100

    Pridavam reseni

---
+++ script.js
@@ -0,0 +1,9 @@
+// Napište funkci max2, která vrátí větší ze dvou zadaných čísel. V JavaScriptu už na toto funkce existuje, jmenuje se Math.max. Pro účely tohoto příkladu se budeme tvářit, že o ní nevíme.
+
+const max2 = (a, b) => {
+    if (a > b) {
+      return a;
+    } else {
+      return b;
+    }
+  };
